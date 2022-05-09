c4d7fdde04a433330155466199cf37fe8af57567
Anton Kharchenko <gumb1t97@gmail.com>
2022-05-09 08:51:39 +0300

    Improved Enum.GetEnumValues

---
+++ RoseX.CLR.Definitions/Enum/RoseXEnumerable.cs
@@ -0,0 +1,14 @@
+﻿namespace RoseX.CLR.Definitions.Enum;
+
+public static class RoseXEnumerable
+{
+    /// <summary>
+    /// This is method repair the standard GetEnumValues. Microsoft method is creating array and it is making allocation. 
+    /// </summary>
+    /// <typeparam name="TEnum"></typeparam>
+    /// <returns></returns>
+    public static TEnum[] GetEnumValues<TEnum>() where TEnum : struct
+    {
+        return (TEnum[])System.Enum.GetValues(typeof(TEnum));
+    }
+}
